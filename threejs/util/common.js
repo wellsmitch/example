124a0d1efb4fde96c06f031d35e31da04a849ff0
songyubin <1970083855@qq.com>
2020-04-09 13:50:58 +0800

    提交了一些代码

---
+++ threejs/util/common.js
@@ -0,0 +1,27 @@
+function common() {
+    var scene = new THREE.Scene();
+
+    var renderer = new THREE.WebGLRenderer();
+
+    renderer.setClearColor(new THREE.Color("#eeeeee"));
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+
+    var paneGeometry = new THREE.PlaneGeometry(100, 60);
+    var planeMaterial = new THREE.MeshLambertMaterial({color: "#9c39fb"});
+    var plane = new THREE.Mesh(paneGeometry, planeMaterial);
+    plane.receiveShadow = true;
+
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.set(15, 0, 0);
+    scene.add(plane);
+    var axes = new THREE.AxesHelper(20);
+    scene.add(axes);
+
+    return {
+        scene: scene,
+        renderer: renderer,
+        plane: plane,
+        camera: new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000)
+    }
+}
